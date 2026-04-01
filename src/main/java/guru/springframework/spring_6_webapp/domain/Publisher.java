8987f020e99578d836b0590692aef86af50323f9
JordyCA <jordycastroavendano@gmail.com>
2026-04-01 14:19:39 -0600

    [add] - Add Publisher Repository and h2 configurations

---
+++ src/main/java/guru/springframework/spring_6_webapp/domain/Publisher.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring_6_webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String publisherName;
+    private String address;
+    private String city;
+    private String zipCode;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
+}
